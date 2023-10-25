f19fce62d647ede57d2432f20dad25964ba20009
Alexander <alethogar@protonmail.com>
2023-10-26 00:12:29 +0200

    Gradle stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,136 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-plugins {
-	id("org.springframework.boot") version "3.1.5"
-	id("io.spring.dependency-management") version "1.1.3"
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
-}
 
 group = "grd.kotlin"
 version = "0.0.1-SNAPSHOT"
+description = "AuthApi"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
-
-repositories {
-	mavenCentral()
+application {
+    mainClass.set("grd.kotlin.authapi.AuthApiApplicationKt")
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	runtimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.8")
+    implementation("org.springframework.boot:spring-boot-starter-security:2.7.8")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.7.8")
+    implementation("org.springframework.boot:spring-boot-starter-validation:2.7.8")
+    implementation("org.springdoc:springdoc-openapi-data-rest:1.6.15")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.15")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.15")
+    runtimeOnly("org.springframework.boot:spring-boot-devtools:2.7.8")
+    // Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.0")
+    implementation("org.owasp:dependency-check-gradle:8.1.2")
+    // Firebase
+    implementation("com.google.firebase:firebase-admin:9.1.1")
+    implementation("com.google.gms:google-services:4.3.15")
+    // JSON
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.14.2")
+    implementation("com.github.java-json-tools:json-patch:1.13")
+    implementation("com.google.code.gson:gson:2.10.1")
+    // JWT
+    implementation("io.jsonwebtoken:jjwt:0.9.1")
+    // Logging
+    implementation("io.klogging:klogging-jvm:0.4.13")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.4")
+    runtimeOnly("org.reactivestreams:reactive-streams:1.0.4")
+    // Test/Testing
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.8")
+    testImplementation("io.rest-assured:rest-assured:5.3.0")
+    testImplementation("io.rest-assured:json-path:5.3.0")
+    testImplementation("io.rest-assured:xml-path:5.3.0")
+    testImplementation("org.testcontainers:testcontainers:1.17.6")
+    // Test/DB
+    testImplementation("com.h2database:h2:2.1.214")
+    // Test/Mock
+    testImplementation("org.mockito:mockito-core:4.11.0")
+    testImplementation("io.rest-assured:spring-mock-mvc:5.3.0")
+    testImplementation("io.mockk:mockk:1.13.4")
+    testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
 }
 
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+plugins {
+    application
+    java
+    jacoco
+    id("org.springframework.boot") version "2.7.4"
+    id("io.spring.dependency-management") version "1.0.14.RELEASE"
+    id("org.owasp.dependencycheck") version "7.2.1"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.spring") version "1.7.10"
+}
+
+apply(plugin = "org.owasp.dependencycheck")
+
+tasks {
+    test {
+        useJUnitPlatform()
+        testLogging.showExceptions = true
+    }
+}
+
+// Compile
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
 }
 
+tasks.withType<JavaCompile>() {
+    options.encoding = "UTF-8"
+}
+
+// Testing
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+val test by tasks.getting(Test::class) {
+    useJUnitPlatform { }
+}
+
+val testCoverage by tasks.registering {
+    group = "verification"
+    description = "Runs the unit tests with coverage."
+
+    dependsOn(":test", ":jacocoTestReport", ":jacocoTestCoverageVerification")
+    val jacocoTestReport = tasks.findByName("jacocoTestReport")
+    jacocoTestReport?.mustRunAfter(tasks.findByName("test"))
+    tasks.findByName("jacocoTestCoverageVerification")?.mustRunAfter(jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = false
+        csv.isEnabled = false
+        html.isEnabled = true
+        html.destination = file("$buildDir/reports/coverage")
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.7".toBigDecimal()
+            }
+        }
+    }
+}
+
+// Build
+tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootJar>().configureEach {
+    exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
+    launchScript()
 }
