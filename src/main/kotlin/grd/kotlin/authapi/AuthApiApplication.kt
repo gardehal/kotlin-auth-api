6d96fbb44e2d4aa05a8711ee0496213c520ae358
Alexander <alethogar@protonmail.com>
2023-10-27 23:41:52 +0200

    Update AuthApiApplication.kt

--- src/main/kotlin/grd/kotlin/authapi/AuthApiApplication.kt
+++ src/main/kotlin/grd/kotlin/authapi/AuthApiApplication.kt
@@ -2,10 +2,35 @@ package grd.kotlin.authapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import io.klogging.logger
 
 @SpringBootApplication
 class AuthApiApplication
 
-fun main(args: Array<String>) {
+object Log
+{
+	val main = logger("main")
+
+	/**
+	 * {function}, {code}, {message}
+	 */
+	fun controllerMsg(): String
+	{
+		return "{function}, {code}, {message}"
+	}
+
+	/**
+	 * {function}, {code}, {message}, {editor}
+	 */
+	fun controllerEditorMsg(): String
+	{
+		return "{function}, {code}, {message}, {editor}"
+	}
+}
+
+suspend fun main(args: Array<String>)
+{
+	Log.main.info("AuthAPI starting...")
+	Log.main.debug("Java version: {jVersion}, Kotlin version: {kVersion}", System.getProperty("java.version"), KotlinVersion.CURRENT)
 	runApplication<AuthApiApplication>(*args)
 }
