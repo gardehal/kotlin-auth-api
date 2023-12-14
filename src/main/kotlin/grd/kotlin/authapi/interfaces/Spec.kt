2fef4b6a2c02a3582b69e04b08b12f09631f0798
Alexander <alethogar@protonmail.com>
2023-12-14 22:50:06 +0100

    Fixed faulty java to kotlin automatic conversion

--- src/main/kotlin/grd/kotlin/authapi/interfaces/Spec.kt
+++ src/main/kotlin/grd/kotlin/authapi/interfaces/Spec.kt
@@ -8,32 +8,20 @@ import org.springframework.data.jpa.domain.Specification
 
 class Spec<T> : Specification<T>
 {
-    private val criteria: SearchCriteria? = null
+    private lateinit var criteria: SearchCriteria
 
     override fun toPredicate(root: Root<T?>, query: CriteriaQuery<*>?, builder: CriteriaBuilder): Predicate?
     {
-        if(criteria.getOperation().equalsIgnoreCase(">"))
-        {
-            return builder.greaterThanOrEqualTo(
-                root.< String > get < kotlin . String ? > criteria.getKey(), criteria.getValue().toString()
-            )
-        } else if(criteria.getOperation().equalsIgnoreCase("<"))
-        {
-            return builder.lessThanOrEqualTo(
-                root.< String > get < kotlin . String ? > criteria.getKey(), criteria.getValue().toString()
-            )
-        } else if(criteria.getOperation().equalsIgnoreCase(":"))
-        {
-            return if(root.get(criteria.getKey()).getJavaType() === String::class.java)
-            {
-                builder.like(
-                    root.< String > get < kotlin . String ? > criteria.getKey(), "%" + criteria.getValue() + "%"
-                )
-            } else
-            {
-                builder.equal(root.get(criteria.getKey()), criteria.getValue())
-            }
-        }
+        if(criteria.getOperation().equals(">"))
+            return builder.greaterThanOrEqualTo(root.get(criteria.getKey()), criteria.getValue().toString())
+        else if(criteria.getOperation().equals("<"))
+            return builder.lessThanOrEqualTo(root.get(criteria.getKey()), criteria.getValue().toString())
+        else if(criteria.getOperation().equals(":"))
+            return if(root.get<String>(criteria.getKey()).javaType === String::class.java)
+                builder.like(root.get(criteria.getKey()), "%" + criteria.getValue() + "%")
+            else
+                builder.equal(root.get<String>(criteria.getKey()), criteria.getValue())
+
         return null
     }
 }
@@ -43,14 +31,6 @@ class SearchCriteria
     private var key: String? = null
     private var operation: String? = null
     private var value: Any? = null
-
-    fun SearchCriteria(key: String?, operation: String?, value: Any?)
-    {
-        super()
-        this.key = key
-        this.operation = operation
-        this.value = value
-    }
 
     fun getKey(): String?
     {
@@ -89,4 +69,39 @@ class SpecSearchCriteria
     private val operation: SearchOperation? = null
     private val value: Any? = null
     val isOrPredicate = false
+}
+
+enum class SearchOperation
+{
+    EQUALITY,
+    NEGATION,
+    GREATER_THAN,
+    LESS_THAN,
+    LIKE,
+    STARTS_WITH,
+    ENDS_WITH,
+    CONTAINS;
+
+    companion object
+    {
+        val SIMPLE_OPERATION_SET = arrayOf(":", "!", ">", "<", "~")
+        const val OR_PREDICATE_FLAG = "'"
+        const val ZERO_OR_MORE_REGEX = "*"
+        const val OR_OPERATOR = "OR"
+        const val AND_OPERATOR = "AND"
+        const val LEFT_PARANTHESIS = "("
+        const val RIGHT_PARANTHESIS = ")"
+        fun getSimpleOperation(input: Char): SearchOperation?
+        {
+            return when(input)
+            {
+                ':' -> EQUALITY
+                '!' -> NEGATION
+                '>' -> GREATER_THAN
+                '<' -> LESS_THAN
+                '~' -> LIKE
+                else -> null
+            }
+        }
+    }
 }
